4f7d1b380a0fe0859c3cec5e3ef3103509c4ded2
Gabriel <gabrielhaubenhofer@gmail.com>
2026-06-28 15:36:36 +0200

    new font, assets, window size

---
+++ wario-ware/minigame_3.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+# honking - press a button to honk at the humans
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
